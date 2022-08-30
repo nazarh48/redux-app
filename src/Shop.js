eb5f9e425ee887801c81e79e2e7c6319310daa9a
nazarh48 <73153841+nazarh48@users.noreply.github.com>
2022-08-30 17:31:03 +0500

    ReduxApp

---
+++ src/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+      <button className="btn btn-primary mx-3">-</button>
+      Add item to the cart
+      <button className="btn btn-primary mx-3">+</button>
+    </div>
+  )
+}
+
+export default Shop
